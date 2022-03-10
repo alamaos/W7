7f8e2bb8d29834ed6fd70e9ee9694d02c2a0e4f9
alamaos <oskari.alamaki@gmail.com>
2022-03-10 20:51:38 +0200

    7.5

--- app/src/main/java/com/example/w7/MainActivity.java
+++ app/src/main/java/com/example/w7/MainActivity.java
@@ -1,22 +1,65 @@
 package com.example.w7;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
+
+import java.io.*;
+
+
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
+    Context context = null;
+
+    EditText tiedosto;
+    EditText syote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = (TextView) findViewById(R.id.textView);
+        context = MainActivity.this;
+        System.out.println("Tiedoston sijainti: " + context.getFilesDir());
+        tiedosto = findViewById(R.id.tiedosto);
+        syote = findViewById(R.id.syote);
     }
-    public void printer(View a){
-        System.out.println("Hello world!");
-        text.setText("Hello world!");
+    public void FileRead(View v){
+        try {
+            InputStream is = openFileInput(String.valueOf(tiedosto.getText()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String a = "";
+            String data = "";
+            while ((a = br.readLine()) != null) {
+                data = data + a;
+            }
+            syote.setText(data);
+            is.close();
+        } catch (IOException e){
+            Log.e("IOException", "Virhe syötteessä");
+        } finally {
+            System.out.println("Luettu");
+        }
     }
+    public void FileWrite(View v){
+        try {
+            OutputStreamWriter osw = new OutputStreamWriter(context.openFileOutput(String.valueOf(tiedosto.getText()), Context.MODE_PRIVATE));
+            String abc = "";
+            abc = String.valueOf(syote.getText());
+            osw.write(abc);
+            osw.close();
+        }catch (IOException e){
+            Log.e("IOException", "Virhe syötteessä");
+        }finally {
+            System.out.println("Kirjoitettu");
+        }
+    }
+
 }
