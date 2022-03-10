8e7bb1803eb1d45d8ee886ca110493715da61518
alamaos <oskari.alamaki@gmail.com>
2022-03-10 15:38:51 +0200

    Initial Commit

--- app/src/main/java/com/example/w7/MainActivity.java
+++ app/src/main/java/com/example/w7/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.w7;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
     }
     public void printer(View a){
         System.out.println("Hello world!");
+        text.setText("Hello world!");
     }
 }
